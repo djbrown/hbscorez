cbdca2410cf0e8395cb7ca613ce19acbf0b74437
Daniel Brown <djbrown@users.noreply.github.com>
2017-09-26 03:26:14 +0200

    created test to check whether the order of the filter in the query matters

--- scorers/tests.py
+++ scorers/tests.py
@@ -1,3 +1,28 @@
+import django
+from django.db.models import Sum, Count
 from django.test import TestCase
 
-# Create your tests here.
+django.setup()
+
+from scorers.models import PlayerScore
+
+
+class ScorersTestCase(TestCase):
+    def test_filter_order_doesnt_matter(self):
+        q1 = PlayerScore.objects \
+            .values('player_name') \
+            .annotate(total=Sum('goals')) \
+            .filter(total__gt=0) \
+            .annotate(scores=Count('player_name')) \
+            .annotate(penalties=Sum('penalty_goals')) \
+            .order_by('-total')
+
+        q2 = PlayerScore.objects \
+            .values('player_name') \
+            .annotate(total=Sum('goals')) \
+            .annotate(scores=Count('player_name')) \
+            .annotate(penalties=Sum('penalty_goals')) \
+            .filter(total__gt=0) \
+            .order_by('-total')
+
+        self.assertEqual(list(q1), list(q2))
