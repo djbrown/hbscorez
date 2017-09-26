3d5323ad07a64c8e4892af41785de43da9143bff
Daniel Brown <djbrown@users.noreply.github.com>
2017-09-26 03:26:40 +0200

    filter out players who didn't score a goal

--- scorers/views.py
+++ scorers/views.py
@@ -8,8 +8,8 @@ def index(request):
     scores = PlayerScore.objects \
         .values('player_name') \
         .annotate(total=Sum('goals')) \
+        .filter(total__gt=0) \
         .annotate(scores=Count('player_name')) \
         .annotate(penalties=Sum('penalty_goals')) \
         .order_by('-total')
     return render(request, 'scorers/scorers.html', {'scores': scores})
-
