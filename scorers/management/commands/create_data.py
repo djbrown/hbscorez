5dc9a46408eabd46b9a3559082c8bfe1e50f637f
Daniel Brown <djbrown@users.noreply.github.com>
2017-10-20 00:44:53 +0200

    renamed option --faster to --exemplary added option --skip-youth create reports directory if it is missing commented out unnecessary association fixed encoding of team and player names skip score if "player" is an official

--- scorers/management/commands/create_data.py
+++ scorers/management/commands/create_data.py
@@ -12,15 +12,21 @@ from scorers.models import District, League, Player, Score, Team, Association
 
 
 class Command(BaseCommand):
-    fast = False
+    exemplary = False
+    skip_youth = False
+    reports_path = settings.BASE_DIR + "/reports/"
 
     def log(self, text: str) -> None:
         self.stdout.write(self.style.SUCCESS(text))
 
     def add_arguments(self, parser):
-        parser.add_argument('--fast', action='store_true')
+        parser.add_argument('--exemplary', action='store_true')
+        parser.add_argument('--skip-youth', action='store_true')
 
     def handle(self, *args, **options):
+        self.exemplary = options['exemplary']
+        self.skip_youth = options['skip_youth']
+
         Score.objects.all().delete()
         Player.objects.all().delete()
         Team.objects.all().delete()
@@ -28,27 +34,27 @@ class Command(BaseCommand):
         District.objects.all().delete()
         Association.objects.all().delete()
 
-        self.fast = options['fast']
+        os.makedirs(self.reports_path, exist_ok=True)
 
-        badHV = Association(name='Badischer Handball-Verband', acronym='BHV', abbreviation='BAD')
-        badHV.save()
-        bayHV = Association(name='Bayerischer Handball-Verband', acronym='BHV', abbreviation='BAY')
-        bayHV.save()
+        bhv = Association(name='Badischer Handball-Verband', acronym='BHV', abbreviation='BAD')
+        bhv.save()
+        # bayHV = Association(name='Bayerischer Handball-Verband', acronym='BHV', abbreviation='BAY')
+        # bayHV.save()
         # todo: shv - südbadischer handballverband
 
         districts = [
-            ('Baden-Württemberg Oberliga', 'BWOL', badHV, 35, 4),
-            ('Badischer Handball-Verband', 'BHV', badHV, 35, 35),
-            ('Bezirk Nord', 'NORD', badHV, 35, 84),
-            ('Bezirk Süd', 'SÜD', badHV, 35, 43),
-            ('Bruchsal', 'BR', badHV, 35, 36),
-            ('Heidelberg', 'HD', badHV, 35, 37),
-            ('Karlsruhe', 'KA', badHV, 35, 38),
-            ('Mannheim', 'MA', badHV, 35, 39),
-            ('Pforzheim', 'PF', badHV, 35, 40),
+            ('Baden-Württemberg Oberliga', 'BWOL', bhv, 35, 4),
+            ('Badischer Handball-Verband', 'BHV', bhv, 35, 35),
+            ('Bezirk Nord', 'NORD', bhv, 35, 84),
+            ('Bezirk Süd', 'SÜD', bhv, 35, 43),
+            ('Bruchsal', 'BR', bhv, 35, 36),
+            ('Heidelberg', 'HD', bhv, 35, 37),
+            ('Karlsruhe', 'KA', bhv, 35, 38),
+            ('Mannheim', 'MA', bhv, 35, 39),
+            ('Pforzheim', 'PF', bhv, 35, 40),
         ]
         for num, data in enumerate(districts):
-            if self.fast and num is not 1:
+            if self.exemplary and num != 1:
                 continue
             self.log('District {}/{}'.format(num + 1, len(districts)))
             self.create_district(*data)
@@ -64,23 +70,28 @@ class Command(BaseCommand):
         }
         response = requests.post(url=url, data=request_data)
 
-        tree = html.fromstring(code(response.text))
+        tree = html.fromstring(response.text.encode('latin-1').decode())
         league_links = tree.xpath('//*[@id="results"]/div/table[2]/tr/td[1]/a')
 
         for num, league_link in enumerate(league_links):
-            if self.fast and num is not 2:
+            if self.exemplary and num != 2:
                 continue
             self.log('  - League {}/{}'.format(num + 1, len(league_links)))
             self.create_league(league_link, district)
 
     def create_league(self, link, district):
+        abbreviation = link.text
+        if self.skip_youth and (abbreviation[:2] == 'mJ' or abbreviation[:2] == 'wJ'):
+            if abbreviation[:2] != 'M-' and abbreviation[:2] != 'F-':
+                self.log(abbreviation)
+            return
+
         url = 'http://spo.handball4all.de/Spielbetrieb/index.php' + link.get('href') + '&all=1'
         response = requests.get(url=url)
 
-        tree = html.fromstring(code(response.text))
+        tree = html.fromstring(response.text.encode("latin-1").decode())
         heading = tree.xpath('//*[@id="results"]/div/h1/text()[2]')[0]
         name = heading.split(' - ')[0]
-        abbreviation = link.text
 
         league = League(name=name, abbreviation=abbreviation, district=district)
         league.save()
@@ -97,19 +108,17 @@ class Command(BaseCommand):
             self.create_scores(game_row, league=league)
 
     def create_scores(self, game_row, league):
-        reports_path = settings.BASE_DIR + "/reports/"
         report_url = game_row.xpath('./td[11]/a/@href')[0]
         params = urlsplit(report_url)[3]
         game_id = parse_qs(params)['sGID'][0]
-        file_path = "{}/{}.pdf".format(reports_path, game_id)
+        file_path = "{}/{}.pdf".format(self.reports_path, game_id)
 
         if not os.path.isfile(file_path):
             response = requests.get(report_url, stream=True)
             with open(file_path, 'wb') as file:
                 file.write(response.content)
 
-        teams_pdf = tabula.read_pdf(file_path, output_format='json', encoding='cp1252',
-                                    **{'pages': 1, 'lattice': True})
+        teams_pdf = tabula.read_pdf(file_path, output_format='json', **{'pages': 1, 'lattice': True})
         team_names = teams_pdf[0]['data'][3][1]['text']
         home_team_name, guest_team_name = self.parse_team_names(team_names)
         scores_pdf = tabula.read_pdf(file_path, output_format='json', encoding='cp1252',
@@ -123,8 +132,8 @@ class Command(BaseCommand):
         table_rows = table['data']
         for table_row in table_rows[2:]:
             row_data = [cell['text'] for cell in table_row]
-            # player_number = row_data[0]
-            player_name = row_data[1]
+            player_number = row_data[0]
+            player_name = row_data[1].encode("cp1252").decode()
             # player_year_of_birth = row_data[2]
             goals_total = row_data[5] or 0
             penalty_tries, penalty_goals = self.parse_penalty_data(row_data[6])
@@ -136,7 +145,7 @@ class Command(BaseCommand):
             # report_time = row_data[12]
             # team_suspension_time = row_data[13]
 
-            if not player_name:
+            if not player_name or player_number in ('A', 'B', 'C', 'D'):
                 continue
 
             player = Player.objects.get_or_create(name=player_name, team=team)[0]
@@ -160,7 +169,3 @@ class Command(BaseCommand):
     def parse_team_names(self, text: str) -> (int, int):
         match = re.match("(.+) - (.+)", text)
         return match.group(1), match.group(2)
-
-
-def code(text: str):
-    return text.encode("latin-1").decode()
