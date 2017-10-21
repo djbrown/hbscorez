0e7c880422d1e11f031878bcbb52a0ee5b4dd5dd
Daniel Brown <djbrown@users.noreply.github.com>
2017-10-22 00:14:14 +0200

    moved scorers view from scorers app to leagues app

--- leagues/urls.py
+++ leagues/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 
-from scorers import views as s
 from . import views
 
 urlpatterns = [
     url(r'^$', views.league, name='index'),
-    url(r'^torjaeger/$', s.league_scorers, name='scorers'),
+    url(r'^torjaeger/$', views.league_scorers, name='scorers'),
 ]
