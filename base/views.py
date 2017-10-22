ca02307f60fbb8c2ab682959a8888c6e61288f0e
Daniel Brown <djbrown@users.noreply.github.com>
2017-10-22 04:48:54 +0200

    created site notice

--- base/views.py
+++ base/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request=request, template_name='base/home.html')
+
+
+def notice(request):
+    return render(request=request, template_name='base/notice.html')
+
+
+def contact(request):
+    return render(request=request, template_name='base/contact.html')
--- hbscorez/urls.py
+++ hbscorez/urls.py
@@ -4,10 +4,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 from associations import views as assoc
-from base.views import home
+from base.views import notice, home, contact
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^impressum', notice, name='notice'),
+    url(r'^kontakt$', contact, name='contact'),
     url(r'^admin/', admin.site.urls),
     url(r'^verbaende/$', assoc.associations, name='assocs'),
     url(r'^(?P<assoc_abbr>[a-z]+)/', include('associations.urls', namespace='assoc')),
