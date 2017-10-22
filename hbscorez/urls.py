92857739b29fd1a29c342afbf1c4f60255c233a3
Daniel Brown <djbrown@users.noreply.github.com>
2017-10-22 06:37:10 +0200

    add slash to end of urls

--- hbscorez/urls.py
+++ hbscorez/urls.py
@@ -8,8 +8,8 @@ from base.views import notice, home, contact
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^impressum', notice, name='notice'),
-    url(r'^kontakt$', contact, name='contact'),
+    url(r'^impressum/$', notice, name='notice'),
+    url(r'^kontakt/$', contact, name='contact'),
     url(r'^admin/', admin.site.urls),
     url(r'^verbaende/$', assoc.associations, name='assocs'),
     url(r'^(?P<assoc_abbr>[a-z]+)/', include('associations.urls', namespace='assoc')),
