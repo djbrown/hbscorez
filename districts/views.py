be052ff611845063fbf6b63ba33418406fd04d35
Daniel Brown <djbrown@users.noreply.github.com>
2017-10-30 22:15:14 +0100

    fixed error, so that districts with non ascii characters are resolved correctly

--- districts/views.py
+++ districts/views.py
@@ -4,5 +4,5 @@ from districts.models import District
 
 
 def district(request, assoc_abbr, dist_abbr):
-    dist = District.objects.filter(abbreviation__iexact=dist_abbr).first()
+    dist = District.objects.filter(abbreviation=dist_abbr.upper()).first()
     return render(request, 'scorers/district.html', {'district': dist})
