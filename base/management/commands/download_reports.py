37afe86da0e6302a082388f801411e30de71c5c3
djbrown <djbrown@users.noreply.github.com>
2018-02-18 01:31:35 +0100

    transfers report download logic into seperate command

--- base/management/commands/download_reports.py
+++ base/management/commands/download_reports.py
@@ -1,10 +1,38 @@
+from pathlib import Path
+
+import requests
+from django.conf import settings
 from django.core.management import BaseCommand
+
+from base.models import Game
+
+reports_root = Path(settings.REPORTS_ROOT)
 
 
 class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('--force-update', '-f', action='store_true',
+                            help='force download and overwrite if report already exists')
+
     def handle(self, *args, **options):
-        pass
+        reports_root.mkdir(parents=True, exist_ok=True)
 
-    def download_report(self, game_number):
-        pass
+        for game in Game.objects.all():
+            if not create_report_path(game).is_file():
+                self.stdout.write('DOWNLOADING Report {}'.format(game.bhv_id))
+                download_report(game)
+            elif options['force_update']:
+                self.stdout.write('REDOWNLOADING Report {}'.format(game.bhv_id))
+                download_report(game)
+            else:
+                self.stdout.write('SKIPPING Report {} (already exists)'.format(game.bhv_id))
 
+
+def create_report_path(game):
+    return reports_root.joinpath(str(game.bhv_id) + '.pdf')
+
+
+def download_report(game):
+    response = requests.get(game.report_url(), stream=True)
+    create_report_path(game).write_bytes(response.content)
