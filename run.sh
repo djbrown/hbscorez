b96a9f60edd2dbdc28b3254b5a72b2a8d407b8a2
djbrown <djbrown@users.noreply.github.com>
2018-02-18 03:01:14 +0100

    applies latest changes to commands in run file

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py setup
-python manage.py import_reports
+python manage.py download_reports
+python manage.py import_scores
 python manage.py runserver 0.0.0.0:8000
