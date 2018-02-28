548ee07ed4b3d6b5cd1c9009eb0cd82353bbdab4
djbrown <djbrown@users.noreply.github.com>
2018-02-28 22:43:40 +0100

    always sets media url via static() since it's disabled if DEBUG=False

--- hbscorez/urls.py
+++ hbscorez/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(base.urls)),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
