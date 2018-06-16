a8116f77c9e06e88a5902b920bc69ea32705d89e
Daniel Brown <djbrown@users.noreply.github.com>
2018-06-16 19:38:08 +0200

    extends BaseTestCase.assert_objects() by an optional filter argument

--- base/tests/base_test_case.py
+++ base/tests/base_test_case.py
@@ -2,7 +2,10 @@ from django.test import TestCase
 
 
 class BaseTestCase(TestCase):
-    def assert_objects(self, model, count=1):
-        objects = model.objects.all()
+    def assert_objects(self, model, count=1, filters=None):
+        if filters is None:
+            filters = {}
+
+        objects = model.objects.filter(**filters)
         self.assertEqual(len(objects), count)
         return objects[0] if count is 1 else objects
