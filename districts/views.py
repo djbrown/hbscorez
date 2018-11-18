d4ef066dd7fd65b96a4d2c4350b41534a8980ac2
Daniel Brown <djbrown@users.noreply.github.com>
2018-11-18 06:56:33 +0100

    display districts leagues by season

--- districts/views.py
+++ districts/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+import collections
+
+from django.shortcuts import get_object_or_404, render
 
 from .models import District
 
 
 def detail(request, bhv_id):
     district = get_object_or_404(District, bhv_id=bhv_id)
-    return render(request, 'districts/detail.j2', {'district': district})
+    leagues = district.league_set
+    grouped = collections.defaultdict(list)
+    for league in leagues.all():
+        grouped[league.season.start_year].append(league)
+    g = collections.OrderedDict(sorted(grouped.items(), key=lambda t: t[0], reverse=True))
+    for leagues in g.values():
+        print(leagues)
+        leagues.sort(key=lambda l: l.name)
+    return render(request, 'districts/detail.j2', {'district': district, 'leagues': g})
