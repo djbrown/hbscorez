479098f90d4cfc6fd4b9214e761505f4cf3768c2
Daniel Brown <djbrown@users.noreply.github.com>
2018-12-06 02:11:11 +0100

    add more django production settings

--- hbscorez/settings_docker_stack.py
+++ hbscorez/settings_docker_stack.py
@@ -2,6 +2,10 @@ from typing import Optional
 
 from .settings import *  # noqa # pylint: disable=all
 
+SECRET_KEY = '12345678901234567890123456789012345678901234567890'  # CHANGEME
+
+DEBUG = False
+
 ALLOWED_HOSTS = ['hbscorez.localhost']
 
 DATABASES = {
@@ -14,5 +18,23 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+CSRF_COOKIE_SECURE = True
+
+X_FRAME_OPTIONS = 'DENY'
+
+SECURE_HSTS_SECONDS = 10
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+# SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+SECURE_HSTS_PRELOAD = True
 
 MATOMO_URL: Optional[str] = 'https://matomo.hbscorez.localhost/'  # CHANGEME
