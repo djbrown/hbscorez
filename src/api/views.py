c9a4a0405724491134de9e5a3f1f76944ea861ca
Daniel Brown <djbrown@users.noreply.github.com>
2019-08-08 02:21:08 +0200

    avoid shadowing global names (functions)

--- src/api/views.py
+++ src/api/views.py
@@ -6,22 +6,22 @@ from leagues.models import League, Season
 
 
 def associations(_):
-    associations = [{
+    light_associations = [{
         'bhvId': a.bhv_id,
         'name': a.name,
         'abbreviation': a.abbreviation,
     } for a in Association.objects.all()]
 
-    return JsonResponse({'associations': associations})
+    return JsonResponse({'associations': light_associations})
 
 
 def association_districts(_, bhv_id):
-    associations = Association.objects.filter(bhv_id=bhv_id)
+    association_results = Association.objects.filter(bhv_id=bhv_id)
 
-    if not associations.exists():
+    if not association_results.exists():
         return JsonResponse({'error': 'No matching Association found.'}, status=404)
 
-    association = associations[0]
+    association = association_results[0]
 
     districts = [{
         'bhvId': district.bhv_id,
@@ -32,30 +32,30 @@ def association_districts(_, bhv_id):
 
 
 def seasons(_):
-    seasons = [{
+    light_seasons = [{
         'startYear': season.start_year
     } for season in Season.objects.all()]
 
-    return JsonResponse({'seasons': seasons})
+    return JsonResponse({'seasons': light_seasons})
 
 
 def district_season_leagues(_, bhv_id, start_year):
-    districts = District.objects.filter(bhv_id=bhv_id)
-    if not districts.exists():
+    district_results = District.objects.filter(bhv_id=bhv_id)
+    if not district_results.exists():
         return JsonResponse({'error': 'No matching District found.'}, status=404)
-    district = districts[0]
+    district = district_results[0]
 
-    seasons = Season.objects.filter(start_year=start_year)
-    if not seasons.exists():
+    season_results = Season.objects.filter(start_year=start_year)
+    if not season_results.exists():
         return JsonResponse({'error': 'No matching Season found.'}, status=404)
-    season = seasons[0]
+    season = season_results[0]
 
-    leagues = [{
+    light_leagues = [{
         'bhvId': league.bhv_id,
         'name': league.name,
     } for league in League.objects.filter(district=district, season=season)]
 
-    return JsonResponse({'leagues': leagues})
+    return JsonResponse({'leagues': light_leagues})
 
 
 def league_teams(_, bhv_id):
@@ -66,10 +66,10 @@ def league_teams(_, bhv_id):
 
     league = leagues[0]
 
-    teams = [{
+    light_teams = [{
         'bhvId': team.bhv_id,
         'name': team.name,
         'shortName': team.short_name,
     } for team in league.team_set.all()]
 
-    return JsonResponse({'teams': teams})
+    return JsonResponse({'teams': light_teams})
