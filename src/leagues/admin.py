a1d946983edaccfd8fb4c749dc4842ae35355187
Daniel Brown <djbrown@users.noreply.github.com>
2020-09-21 09:08:07 +0200

    make Leagues searchable in Admin panel

--- src/leagues/admin.py
+++ src/leagues/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import League, Season
 
-admin.site.register(League)
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'abbreviation', 'district__name', 'district__bhv_id', 'season__start_year', 'bhv_id')
+
+
 admin.site.register(Season)
