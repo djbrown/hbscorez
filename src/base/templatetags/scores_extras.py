0a6f0390a20cfd207970e50654ebc17f0bfb335f
Daniel Brown <djbrown@users.noreply.github.com>
2020-09-27 22:05:40 +0200

    upgrade to django 3

--- src/base/templatetags/scores_extras.py
+++ src/base/templatetags/scores_extras.py
@@ -1,4 +1,4 @@
-from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.templatetags.static import static
 
 from games.models import GameOutcome, TeamOutcome
 from teams.models import Team
