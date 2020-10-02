adb309be97047b71cfd828b8ea8ce83955777c87
Daniel Brown <djbrown@users.noreply.github.com>
2020-10-02 03:49:37 +0200

    make Games searchable in Admin panel

--- src/games/admin.py
+++ src/games/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Game
 
-admin.site.register(Game)
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    search_fields = ('number', 'home_team__name', 'guest_team__name', 'league__name', 'league__district__name',
+                     'league__district__bhv_id', 'league__season__start_year', 'league__bhv_id')
