3047da9a65e8b291f92fd1091deaf4eebb405564
Daniel Brown <djbrown@users.noreply.github.com>
2020-10-03 15:32:58 +0200

    rename url notice to imprint

--- src/base/urls.py
+++ src/base/urls.py
@@ -7,7 +7,7 @@ app_name = 'base'
 
 urlpatterns = [  # pylint: disable=invalid-name
     path('', TemplateView.as_view(template_name="base/home.j2"), name='home'),
-    path('impressum/', TemplateView.as_view(template_name="base/imprint.j2"), name='notice'),
+    path('impressum/', TemplateView.as_view(template_name="base/imprint.j2"), name='imprint'),
     path('datenschutz/', TemplateView.as_view(template_name="base/privacy.j2"), name='privacy'),
     path('kontakt/', ContactView.as_view(), name="contact_form"),
     path(route='kontakt/gesendet/', name="contact_form_sent",
