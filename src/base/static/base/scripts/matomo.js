bdd91d96d45e401d58e4c5118b0470fb53a39c72
Daniel Brown <djbrown@users.noreply.github.com>
2020-10-16 00:17:50 +0200

    clean up matomo.js

--- src/base/static/base/scripts/matomo.js
+++ src/base/static/base/scripts/matomo.js
@@ -1,11 +1,14 @@
 var _paq = _paq || [];
-// _paq.push(['requireConsent']);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-    var u = MATOMO_URL;
-    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+    _paq.push(['setTrackerUrl', `${MATOMO_URL}piwik.php`]);
     _paq.push(['setSiteId', '1']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'piwik.js'; s.parentNode.insertBefore(g, s);
+    var $script = document.createElement('script');
+    var $lastScript = document.getElementsByTagName('script')[0];
+    $script.type = 'text/javascript';
+    $script.async = true;
+    $script.defer = true;
+    $script.src = `${MATOMO_URL}piwik.js`;
+    $lastScript.parentNode.insertBefore($script, $lastScript);
 })();
