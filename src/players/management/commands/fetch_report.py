b6cab5575e95ff063cb4406a16405173f1eee9ce
Daniel Brown <djbrown@users.noreply.github.com>
2021-05-08 01:58:04 +0200

    return value or raise error (not none)

--- src/players/management/commands/fetch_report.py
+++ src/players/management/commands/fetch_report.py
@@ -7,15 +7,15 @@ import requests
 
 from games.models import Game
 
-MAX_RETRY_DURATION: timedelta = timedelta(hours=3)
+MAX_RETRY_DURATION: timedelta = timedelta(hours=1)
 RETRY_DURATIONS: List[timedelta] = [
     timedelta(seconds=10),
     timedelta(seconds=30),
     timedelta(minutes=1),
     timedelta(minutes=5),
+    timedelta(minutes=10),
     timedelta(minutes=30),
-    timedelta(hours=1),
-    MAX_RETRY_DURATION
+    MAX_RETRY_DURATION,
 ]
 
 LOGGER = logging.getLogger('hbscorez')
@@ -27,7 +27,7 @@ def fetch_report(game: Game):
             return requests.get(game.report_source_url(), stream=True)
         except requests.exceptions.ConnectionError as ex:
             LOGGER.warning('Could not fetch report %s', game)
-            if retry_duration == MAX_RETRY_DURATION:
-                raise ex
-            LOGGER.debug('Now wating for %s', retry_duration)
-            time.sleep(retry_duration.total_seconds())
+            if retry_duration <= MAX_RETRY_DURATION:
+                LOGGER.debug('Now wating for %s', retry_duration)
+                time.sleep(retry_duration.total_seconds())
+    raise ex
