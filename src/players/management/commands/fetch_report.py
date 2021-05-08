4a7e02e26e26b8ff2b396f121311af2b9efd3f05
Daniel Brown <djbrown@users.noreply.github.com>
2021-05-08 03:05:06 +0200

    return value or raise error by recursion

--- src/players/management/commands/fetch_report.py
+++ src/players/management/commands/fetch_report.py
@@ -22,12 +22,18 @@ LOGGER = logging.getLogger('hbscorez')
 
 
 def fetch_report(game: Game):
-    for retry_duration in RETRY_DURATIONS:
-        try:
-            return requests.get(game.report_source_url(), stream=True)
-        except requests.exceptions.ConnectionError as ex:
-            LOGGER.warning('Could not fetch report %s', game)
-            if retry_duration <= MAX_RETRY_DURATION:
-                LOGGER.debug('Now wating for %s', retry_duration)
-                time.sleep(retry_duration.total_seconds())
-    raise ex
+    return retry_downloads(game, RETRY_DURATIONS)
+
+
+def retry_downloads(game: Game, retry_durations: List[timedelta]):
+    try:
+        url = game.report_source_url()
+        return requests.get(url, stream=True)
+    except requests.exceptions.ConnectionError as exc:
+        if len(retry_durations) == 0:
+            raise exc
+        retry_duration = retry_durations[0]
+        LOGGER.warning('Could not fetch report %s', game)
+        LOGGER.debug('Now wating for %s', retry_duration)
+        time.sleep(retry_duration.total_seconds())
+        return retry_downloads(game, retry_durations[1:])
