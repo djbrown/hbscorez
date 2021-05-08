ff05ea20e9fd2b54f348e78c3f7546011dd8ef0f
Daniel Brown <djbrown@users.noreply.github.com>
2021-05-08 02:13:33 +0200

    update to new version of matomo

--- src/base/static/base/scripts/matomo.js
+++ src/base/static/base/scripts/matomo.js
@@ -1,14 +1,13 @@
-var _paq = _paq || [];
+var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-    _paq.push(['setTrackerUrl', `${MATOMO_URL}piwik.php`]);
+    _paq.push(['setTrackerUrl', `${MATOMO_URL}matomo.php`]);
     _paq.push(['setSiteId', '1']);
     var $script = document.createElement('script');
     var $lastScript = document.getElementsByTagName('script')[0];
     $script.type = 'text/javascript';
     $script.async = true;
-    $script.defer = true;
-    $script.src = `${MATOMO_URL}piwik.js`;
+    $script.src = `${MATOMO_URL}matomo.js`;
     $lastScript.parentNode.insertBefore($script, $lastScript);
 })();
