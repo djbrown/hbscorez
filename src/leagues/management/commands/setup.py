52ec1ea3fe5fe05e8b423b393a3efb04cb855e18
Daniel Brown <djbrown@users.noreply.github.com>
2021-10-21 01:03:41 +0200

    ignore Vorbereitungsligen

--- src/leagues/management/commands/setup.py
+++ src/leagues/management/commands/setup.py
@@ -162,7 +162,8 @@ class Command(BaseCommand):
         name = parsing.parse_league_name(dom)
 
         if any(n in name for n in ['Platzierungsrunde', 'Meister', 'Freiwurf', 'Maxi',
-                                   'turnier', 'wettbewerb', 'pokal', 'Test', 'F-FS', 'M-FS']):
+                                   'turnier', 'wettbewerb', 'pokal', 'Test', 'F-FS', 'M-FS',
+                                   'Vorbereitung']):
             LOGGER.debug('SKIPPING League (name): %s %s', bhv_id, name)
             return
 
