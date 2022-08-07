984ee0c721975e6d2fd9e3e5d139c07385bcab04
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-07 18:49:34 +0200

    use const instead of var

--- src/base/static/base/scripts/matomo.js
+++ src/base/static/base/scripts/matomo.js
@@ -1,11 +1,11 @@
-var _paq = window._paq = window._paq || [];
+const _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
     _paq.push(['setTrackerUrl', `${MATOMO_URL}matomo.php`]);
     _paq.push(['setSiteId', '1']);
-    var $script = document.createElement('script');
-    var $lastScript = document.getElementsByTagName('script')[0];
+    const $script = document.createElement('script');
+    const $lastScript = document.getElementsByTagName('script')[0];
     $script.type = 'text/javascript';
     $script.async = true;
     $script.src = `${MATOMO_URL}matomo.js`;
