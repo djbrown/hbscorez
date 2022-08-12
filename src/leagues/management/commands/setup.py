d43dc42565770641b5d4186384c37229e2b8862d
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-13 01:12:48 +0200

    rafactor setup command: move methods out of class

--- src/leagues/management/commands/setup.py
+++ src/leagues/management/commands/setup.py
@@ -41,176 +41,182 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         options['processed_districts'] = set()
         env.UPDATING.set_value(Value.TRUE)
-        self.create_associations(options)
+        create_associations(options)
         env.UPDATING.set_value(Value.FALSE)
 
-    def create_associations(self, options):
-        url = settings.NEW_ROOT_SOURCE_URL
-        dom = logic.get_html(url)
-        portal_paths = dom.xpath('//div[@id="main-content"]//table[@summary]/tbody/tr/td[1]/a/@href')
-        for portal_path in portal_paths:
-            portal_url = portal_path if portal_path.startswith('http') else settings.NEW_ROOT_SOURCE_URL + portal_path
-            bhv_id = self.get_association_bhv_id(portal_url)
-            try:
-                self.create_association(bhv_id, options)
-            except Exception:
-                logging.getLogger('mail').exception("Could not create Association")
 
-    def get_association_bhv_id(self, association_portal_url: str) -> int:
-        dom = logic.get_html(association_portal_url)
-        [bhv_id] = dom.xpath('//div[@id="app"]/@data-og-id')
-        return int(bhv_id)
-
-    def create_association(self, bhv_id, options):
-        url = Association.build_source_url(bhv_id)
-        dom = logic.get_html(url)
-
-        name = parsing.parse_association_name(dom)
+def create_associations(options):
+    url = settings.NEW_ROOT_SOURCE_URL
+    dom = logic.get_html(url)
+    portal_paths = dom.xpath('//div[@id="main-content"]//table[@summary]/tbody/tr/td[1]/a/@href')
+    for portal_path in portal_paths:
+        portal_url = portal_path if portal_path.startswith('http') else settings.NEW_ROOT_SOURCE_URL + portal_path
+        bhv_id = get_association_bhv_id(portal_url)
         try:
-            abbreviation = Association.get_association_abbreviation(name)
-        except KeyError:
-            LOGGER.warning("No abbreviation for association '%s'", name)
-            return
+            create_association(bhv_id, options)
+        except Exception:
+            logging.getLogger('mail').exception("Could not create Association")
 
-        if options['associations'] and bhv_id not in options['associations']:
-            LOGGER.debug('SKIPPING Association (options): %s %s', bhv_id, name)
-            return
 
-        association, created = Association.objects.get_or_create(name=name, abbreviation=abbreviation, bhv_id=bhv_id)
-        if created:
-            LOGGER.info('CREATED Association: %s', association)
-        else:
-            LOGGER.info('EXISTING Association: %s', association)
+def get_association_bhv_id(association_portal_url: str) -> int:
+    dom = logic.get_html(association_portal_url)
+    [bhv_id] = dom.xpath('//div[@id="app"]/@data-og-id')
+    return int(bhv_id)
 
-        items = dom.xpath('//select[@name="orgID"]/option[position()>1]')
-        for item in items:
-            try:
-                self.create_district(item, association, options)
-            except Exception:
-                logging.getLogger('mail').exception("Could not create District")
 
-    def create_district(self, district_item, association: Association, options):
-        name = district_item.text
-        bhv_id = int(district_item.get('value'))
+def create_association(bhv_id, options):
+    url = Association.build_source_url(bhv_id)
+    dom = logic.get_html(url)
 
-        if options['districts'] and bhv_id not in options['districts']:
-            LOGGER.debug('SKIPPING District (options): %s %s', bhv_id, name)
-            return
+    name = parsing.parse_association_name(dom)
+    try:
+        abbreviation = Association.get_association_abbreviation(name)
+    except KeyError:
+        LOGGER.warning("No abbreviation for association '%s'", name)
+        return
 
-        district, created = District.objects.get_or_create(name=name, bhv_id=bhv_id)
-        district.associations.add(association)
-        if bhv_id in options['processed_districts']:
-            LOGGER.debug('SKIPPING District: %s %s (already processed)', bhv_id, name)
-            return
+    if options['associations'] and bhv_id not in options['associations']:
+        LOGGER.debug('SKIPPING Association (options): %s %s', bhv_id, name)
+        return
 
-        if created:
-            LOGGER.info('CREATED District: %s', district)
-        else:
-            LOGGER.info('EXISTING District: %s', district)
-        options['processed_districts'].add(bhv_id)
+    association, created = Association.objects.get_or_create(name=name, abbreviation=abbreviation, bhv_id=bhv_id)
+    if created:
+        LOGGER.info('CREATED Association: %s', association)
+    else:
+        LOGGER.info('EXISTING Association: %s', association)
 
-        for start_year in range(2004, datetime.now().year + 1):
-            try:
-                self.create_season(district, start_year, options)
-            except Exception:
-                logging.getLogger('mail').exception("Could not create Season")
-
-    def create_season(self, district, start_year, options):
-        if options['seasons'] and start_year not in options['seasons']:
-            LOGGER.debug('SKIPPING Season (options): %s', start_year)
-            return
-
-        season, season_created = Season.objects.get_or_create(start_year=start_year)
-        if season_created:
-            LOGGER.info('CREATED Season: %s', season)
-        else:
-            LOGGER.info('EXISTING Season: %s', season)
-
-        for start_date in [date(start_year, 10, 1) + timedelta(days=10 * n) for n in range(4)]:
-            LOGGER.debug('trying District Season: %s %s %s', district, season, start_date)
-            url = District.build_source_url(district.bhv_id, start_date)
-            dom = logic.get_html(url)
-            league_links = dom.xpath('//div[@id="results"]/div/table[2]/tr/td[1]/a')
-            if league_links:
-                break
-        else:
-            LOGGER.warning('District Season without Leagues: %s %s', district, season)
-            return
-
-        for league_link in league_links:
-            try:
-                self.create_league(league_link, district, season, options)
-            except Exception:
-                logging.getLogger('mail').exception("Could not create League")
-
-    @transaction.atomic
-    def create_league(self, league_link, district, season, options):
-        abbreviation = league_link.text
-        bhv_id = parsing.parse_league_bhv_id(league_link)
-
-        if bhv_id in BUGGED_LEAGUES:
-            LOGGER.debug('SKIPPING League (ignore list): %s %s', bhv_id, abbreviation)
-            return
-
-        if options['leagues'] and bhv_id not in options['leagues']:
-            LOGGER.debug('SKIPPING League (options): %s %s', bhv_id, abbreviation)
-            return
-
-        if abbreviation == 'TEST':
-            LOGGER.debug('SKIPPING League (test league): %s %s', bhv_id, abbreviation)
-            return
-
-        url = League.build_source_url(bhv_id)
-        dom = logic.get_html(url)
-        name = parsing.parse_league_name(dom)
-
-        irrelevant_league_name_indicators = [
-            'Platzierungsrunde',
-            'Kreisvergleichsspiele',
-            'pokal', 'Pokal', 'Trophy',
-            'Vorbereitung', 'F-FS', 'M-FS', 'Quali',
-            'Freiwurf', 'Maxi', 'turnier', 'wettbewerb',
-            'Test', 'Planung', 'planung',
-        ]
-
-        if any(n in name for n in irrelevant_league_name_indicators):
-            LOGGER.debug('SKIPPING League (name): %s %s', bhv_id, name)
-            return
-
-        team_links = parsing.parse_team_links(dom)
-        if not team_links:
-            LOGGER.debug('SKIPPING League (no team table): %s %s', bhv_id, name)
-            return
-
-        game_rows = parsing.parse_game_rows(dom)
-        if not game_rows:
-            LOGGER.debug('SKIPPING League (no games): %s %s', bhv_id, name)
-            return
-
+    items = dom.xpath('//select[@name="orgID"]/option[position()>1]')
+    for item in items:
         try:
-            name = LeagueName.objects.get(bhv_id=bhv_id).name
-        except LeagueName.DoesNotExist:
-            pass
+            create_district(item, association, options)
+        except Exception:
+            logging.getLogger('mail').exception("Could not create District")
 
-        if League.is_youth(abbreviation, name) and not options['youth']:
-            LOGGER.debug('SKIPPING League (youth league): %s %s %s', bhv_id, abbreviation, name)
-            return
 
-        league, league_created = League.objects.get_or_create(
-            name=name, abbreviation=abbreviation, district=district, season=season, bhv_id=bhv_id)
-        if league_created:
-            LOGGER.info('CREATED League: %s', league)
-        else:
-            LOGGER.info('EXISTING League: %s', league)
+def create_district(district_item, association: Association, options):
+    name = district_item.text
+    bhv_id = int(district_item.get('value'))
 
-        if options['skip_teams']:
-            return
+    if options['districts'] and bhv_id not in options['districts']:
+        LOGGER.debug('SKIPPING District (options): %s %s', bhv_id, name)
+        return
 
-        for team_link in team_links:
-            create_team(team_link, league)
+    district, created = District.objects.get_or_create(name=name, bhv_id=bhv_id)
+    district.associations.add(association)
+    if bhv_id in options['processed_districts']:
+        LOGGER.debug('SKIPPING District: %s %s (already processed)', bhv_id, name)
+        return
 
-        retirements = parsing.parse_retirements(dom)
-        check_retirements(retirements, league)
+    if created:
+        LOGGER.info('CREATED District: %s', district)
+    else:
+        LOGGER.info('EXISTING District: %s', district)
+    options['processed_districts'].add(bhv_id)
+
+    for start_year in range(2004, datetime.now().year + 1):
+        try:
+            create_season(district, start_year, options)
+        except Exception:
+            logging.getLogger('mail').exception("Could not create Season")
+
+
+def create_season(district, start_year, options):
+    if options['seasons'] and start_year not in options['seasons']:
+        LOGGER.debug('SKIPPING Season (options): %s', start_year)
+        return
+
+    season, season_created = Season.objects.get_or_create(start_year=start_year)
+    if season_created:
+        LOGGER.info('CREATED Season: %s', season)
+    else:
+        LOGGER.info('EXISTING Season: %s', season)
+
+    for start_date in [date(start_year, 10, 1) + timedelta(days=10 * n) for n in range(4)]:
+        LOGGER.debug('trying District Season: %s %s %s', district, season, start_date)
+        url = District.build_source_url(district.bhv_id, start_date)
+        dom = logic.get_html(url)
+        league_links = dom.xpath('//div[@id="results"]/div/table[2]/tr/td[1]/a')
+        if league_links:
+            break
+    else:
+        LOGGER.warning('District Season without Leagues: %s %s', district, season)
+        return
+
+    for league_link in league_links:
+        try:
+            create_league(league_link, district, season, options)
+        except Exception:
+            logging.getLogger('mail').exception("Could not create League")
+
+
+@transaction.atomic
+def create_league(league_link, district, season, options):
+    abbreviation = league_link.text
+    bhv_id = parsing.parse_league_bhv_id(league_link)
+
+    if bhv_id in BUGGED_LEAGUES:
+        LOGGER.debug('SKIPPING League (ignore list): %s %s', bhv_id, abbreviation)
+        return
+
+    if options['leagues'] and bhv_id not in options['leagues']:
+        LOGGER.debug('SKIPPING League (options): %s %s', bhv_id, abbreviation)
+        return
+
+    if abbreviation == 'TEST':
+        LOGGER.debug('SKIPPING League (test league): %s %s', bhv_id, abbreviation)
+        return
+
+    url = League.build_source_url(bhv_id)
+    dom = logic.get_html(url)
+    name = parsing.parse_league_name(dom)
+
+    irrelevant_league_name_indicators = [
+        'Platzierungsrunde',
+        'Kreisvergleichsspiele',
+        'pokal', 'Pokal', 'Trophy',
+        'Vorbereitung', 'F-FS', 'M-FS', 'Quali',
+        'Freiwurf', 'Maxi', 'turnier', 'wettbewerb',
+        'Test', 'Planung', 'planung',
+    ]
+
+    if any(n in name for n in irrelevant_league_name_indicators):
+        LOGGER.debug('SKIPPING League (name): %s %s', bhv_id, name)
+        return
+
+    team_links = parsing.parse_team_links(dom)
+    if not team_links:
+        LOGGER.debug('SKIPPING League (no team table): %s %s', bhv_id, name)
+        return
+
+    game_rows = parsing.parse_game_rows(dom)
+    if not game_rows:
+        LOGGER.debug('SKIPPING League (no games): %s %s', bhv_id, name)
+        return
+
+    try:
+        name = LeagueName.objects.get(bhv_id=bhv_id).name
+    except LeagueName.DoesNotExist:
+        pass
+
+    if League.is_youth(abbreviation, name) and not options['youth']:
+        LOGGER.debug('SKIPPING League (youth league): %s %s %s', bhv_id, abbreviation, name)
+        return
+
+    league, league_created = League.objects.get_or_create(
+        name=name, abbreviation=abbreviation, district=district, season=season, bhv_id=bhv_id)
+    if league_created:
+        LOGGER.info('CREATED League: %s', league)
+    else:
+        LOGGER.info('EXISTING League: %s', league)
+
+    if options['skip_teams']:
+        return
+
+    for team_link in team_links:
+        create_team(team_link, league)
+
+    retirements = parsing.parse_retirements(dom)
+    check_retirements(retirements, league)
 
 
 def create_team(link, league):
