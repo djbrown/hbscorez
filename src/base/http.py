496d43981801d8a8711b40b6fe10520410f39328
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-13 11:44:08 +0200

    rafactor http: remove unused code

--- src/base/http.py
+++ src/base/http.py
@@ -2,12 +2,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
-retry_strategy = Retry(
-    read=3,
-    status_forcelist=[429, 500, 502, 503, 504],
-    allowed_methods=["GET"],
-    backoff_factor=10,
-)
-adapter = HTTPAdapter(max_retries=Retry())
 http = requests.Session()
-http.mount("https://", adapter)
+http.mount("https://",  HTTPAdapter(max_retries=Retry()))
+
+
