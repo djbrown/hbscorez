d04c078db6d99be0fbf543dd761ab943240fbf62
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-17 22:07:26 +0200

    positive integrationtest import_report

--- src/players/tests/integration/test_import_reports.py
+++ src/players/tests/integration/test_import_reports.py
@@ -1,8 +1,32 @@
+from datetime import timedelta
 from unittest.mock import Mock, patch
 
 from base.tests.base import IntegrationTestCase
 from games.tests.integration import test_import_games
 from players.models import Player, ReportsBlacklist, Score
+
+
+class TestImportReport(IntegrationTestCase):
+    def test_successful(self):
+        self.assert_command('setup', '-a', 77, '-d', 77, '-s', 2021, '-l', 77606)
+        self.assert_command('import_games', '-g', 10001127)
+        self.assert_command('import_reports')
+
+        scores = self.assert_objects(Score, count=28)
+        self.assertEqual(scores[0].game.report_number, 1525036)
+        self.assertEqual(scores[0].game.spectators, 251)
+
+        score: Score = scores.get(player_number=24)
+        self.assertEqual(score.goals, 10)
+        self.assertEqual(score.penalty_goals, 7)
+        self.assertEqual(score.penalty_tries, 7)
+        self.assertEqual(score.warning_time, timedelta(minutes=7, seconds=7))
+        self.assertEqual(score.first_suspension_time, timedelta(minutes=25, seconds=1))
+        self.assertEqual(score.second_suspension_time, None)
+        self.assertEqual(score.third_suspension_time, None)
+        self.assertEqual(score.disqualification_time, None)
+        self.assertEqual(score.report_time, None)
+        self.assertEqual(score.team_suspension_time, None)
 
 
 class Forfeit(IntegrationTestCase):
