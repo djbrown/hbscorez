deea10356e8356def64bc3dcfbc803f66707d0ea
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-17 20:58:14 +0200

    setup heroku-user in docker image

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM python:3.10-bullseye
 
-RUN mkdir /code
-WORKDIR /code
-
-RUN pip install pipenv==2022.1.8
 RUN apt-get update \
     && apt-get install -y --no-install-recommends default-jre fonts-liberation gsfonts locales \
     && apt-get clean \
@@ -12,12 +8,29 @@ RUN apt-get update \
 RUN echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen \
     && locale-gen
 
+RUN pip install pipenv==2022.1.8 gunicorn==20.1.0
+
+ENV HOME=/code
+RUN mkdir $HOME
+WORKDIR $HOME
+RUN chown -R 1001 $HOME \
+    && chgrp -R 1001 $HOME \
+    && chmod -R g+rwx $HOME
+
+USER 1001:1001
+
 COPY Pipfile .
 COPY Pipfile.lock .
 RUN pipenv install --deploy
-RUN pipenv install --skip-lock gunicorn==20.1.0
+# RUN pipenv install --skip-lock gunicorn==20.1.0
 
 COPY entrypoint.sh .
 COPY src/ src/
+
+# USER root
+# RUN touch $HOME/hbscorez.log
+# RUN chgrp -R 1001 $HOME/hbscorez.log \
+#     && chown -R o+rwx $HOME/hbscorez.log
+# USER 1001:1001
 
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
 
-mkdir -p logs
-touch ./logs/gunicorn.log
-touch ./logs/gunicorn-access.log
-tail -n 0 -f ./logs/gunicorn*.log &
+cd $HOME
+
+id
+# usermod -aG 1001 $USER
+# id
+# Override user ID lookup to cope with being randomly assigned IDs using
+# the -u option to 'docker run'.
+
+USER_ID=$(id -u)
+
+if [ x"$USER_ID" != x"0" -a x"$USER_ID" != x"1001" ]; then
+    NSS_WRAPPER_PASSWD=/tmp/passwd.nss_wrapper
+    NSS_WRAPPER_GROUP=/etc/group
+
+    cat /etc/passwd | sed -e 's/^hbscorez:/builder:/' > $NSS_WRAPPER_PASSWD
+
+    echo "hbscorez:x:$USER_ID:0:HbScorez,,,:/code:/bin/bash" >> $NSS_WRAPPER_PASSWD
+
+    export NSS_WRAPPER_PASSWD
+    export NSS_WRAPPER_GROUP
+
+    LD_PRELOAD=/usr/local/lib64/libnss_wrapper.so
+    export LD_PRELOAD
+fi
+id
+
+touch /code/hbscorez.log
+# mkdir -p logs
+# touch ./logs/gunicorn.log
+# touch ./logs/gunicorn-access.log
+# tail -n 0 -f ./logs/gunicorn*.log &
 
 SETTINGS_FILE="/settings.py"
 if [ -f "$SETTINGS_FILE" ]
@@ -14,7 +41,7 @@ then
 fi
 
 export PYTHONUNBUFFERED=1
-pipenv run ./src/manage.py migrate || { echo "could not apply migrations. stopping startup.. "; exit 5; }
+pipenv run ./src/manage.py migrate & ps fauxwww & ls -la
 pipenv run ./src/manage.py correct_data || { echo "could not correct_data. stopping startup.. "; exit 6; }
 
 if [[ -z "${GUNICORN_WORKERS}" ]]; then
