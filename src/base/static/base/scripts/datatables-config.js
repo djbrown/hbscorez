ba447b140d1aec859f947724b7d63f71e3ccfdb6
Daniel Brown <djbrown@users.noreply.github.com>
2022-08-22 22:53:29 +0200

    reduce function length

--- src/base/static/base/scripts/datatables-config.js
+++ src/base/static/base/scripts/datatables-config.js
@@ -8,41 +8,43 @@ $(document).ready(function () {
         return;
     }
 
+    const language = {
+        //"url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
+        "sEmptyTable": "Keine Daten in der Tabelle vorhanden",
+        "sInfo": "_START_ bis _END_ von _TOTAL_ Einträgen",
+        "sInfoEmpty": "0 bis 0 von 0 Einträgen",
+        "sInfoFiltered": "(gefiltert von _MAX_ Einträgen)",
+        "sInfoPostFix": "",
+        "sInfoThousands": ".",
+        "sLengthMenu": "_MENU_ Einträge anzeigen",
+        "sLoadingRecords": "Wird geladen...",
+        "sProcessing": "Bitte warten...",
+        "sSearch": "Suchen",
+        "sZeroRecords": "Keine Einträge vorhanden.",
+        "oPaginate": {
+            "sFirst": "Erste",
+            "sPrevious": "Zurück",
+            "sNext": "Nächste",
+            "sLast": "Letzte"
+        },
+        "oAria": {
+            "sSortAscending": ": aktivieren, um Spalte aufsteigend zu sortieren",
+            "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+        },
+        select: {
+            rows: {
+                _: '%d Zeilen ausgewählt',
+                0: 'Zum Auswählen auf eine Zeile klicken',
+                1: '1 Zeile ausgewählt'
+            }
+        }
+    }
+
     function initTable(paging = true) {
         return $table.DataTable({
             paging,
             "colReorder": true,
-            "language": {
-                //"url": "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/German.json"
-                "sEmptyTable": "Keine Daten in der Tabelle vorhanden",
-                "sInfo": "_START_ bis _END_ von _TOTAL_ Einträgen",
-                "sInfoEmpty": "0 bis 0 von 0 Einträgen",
-                "sInfoFiltered": "(gefiltert von _MAX_ Einträgen)",
-                "sInfoPostFix": "",
-                "sInfoThousands": ".",
-                "sLengthMenu": "_MENU_ Einträge anzeigen",
-                "sLoadingRecords": "Wird geladen...",
-                "sProcessing": "Bitte warten...",
-                "sSearch": "Suchen",
-                "sZeroRecords": "Keine Einträge vorhanden.",
-                "oPaginate": {
-                    "sFirst": "Erste",
-                    "sPrevious": "Zurück",
-                    "sNext": "Nächste",
-                    "sLast": "Letzte"
-                },
-                "oAria": {
-                    "sSortAscending": ": aktivieren, um Spalte aufsteigend zu sortieren",
-                    "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
-                },
-                select: {
-                    rows: {
-                        _: '%d Zeilen ausgewählt',
-                        0: 'Zum Auswählen auf eine Zeile klicken',
-                        1: '1 Zeile ausgewählt'
-                    }
-                }
-            }
+            "language": language
         });
     }
 
