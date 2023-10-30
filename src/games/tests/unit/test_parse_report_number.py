0f716c83fc10db137c8c60958a41860a0ac608e8
Daniel Brown <djbrown@users.noreply.github.com>
2023-10-30 20:24:46 +0000

    cleanup tests

--- src/games/tests/unit/test_parse_report_number.py
+++ src/games/tests/unit/test_parse_report_number.py
@@ -10,18 +10,18 @@ class ParseReportNumberTest(TestCase):
         actual = parsing.parse_report_number(dom)
         self.assertEqual(expected, actual)
 
-    def test__empty_cell_markup__returns__none(self):
+    def test_empty(self):
         markup = '<td></td>'
         expected = None
         self.assert_from_markup(markup, expected)
 
-    def test__usual_markup__returns__correct_number(self):
+    def test_number(self):
         markup = '<td><a href="https://spo.handball4all.de/misc/sboPublicReports.php?sGID=123456"' \
                  ' target="_blank">PI</a>  </td>'
         expected = 123456
         self.assert_from_markup(markup, expected)
 
-    def test__rescheduled_markup__returns__correct_number(self):
+    def test_rescheduled(self):
         markup = '<td><a href="https://spo.handball4all.de/misc/sboPublicReports.php?sGID=123456"' \
                  ' target="_blank">PI</a> <a style="cursor:help" title="geä. Anspielzeit">geä..</a></td>'
         expected = 123456
