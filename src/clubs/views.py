48cf95df576b1d1da216782ab9a1aa04aef5d2ad
Daniel Brown <djbrown@users.noreply.github.com>
2024-03-29 15:12:26 +0000

    display teams on club details page

--- src/clubs/views.py
+++ src/clubs/views.py
@@ -1,8 +1,15 @@
+from collections import defaultdict
+
 from django.shortcuts import get_object_or_404, render
 
 from clubs.models import Club
+from teams.models import Team
 
 
 def detail(request, bhv_id):
     club = get_object_or_404(Club, bhv_id=bhv_id)
-    return render(request, 'clubs/detail.j2', {'club': club})
+    teams_by_season = defaultdict(list)
+    team: Team
+    for team in club.team_set.order_by('-league__season__start_year', 'name', 'league__abbreviation'):
+        teams_by_season[team.league.season.start_year].append(team)
+    return render(request, 'clubs/detail.j2', {'club': club, 'teams_by_season': teams_by_season})
