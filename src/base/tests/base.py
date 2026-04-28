cc087902f5ea31e55b54dfdc107d2ab7f559cafe
Daniel Brown <djbrown@users.noreply.github.com>
2026-04-28 20:42:39 +0200

    fix import for pylint

--- src/base/tests/base.py
+++ src/base/tests/base.py
@@ -15,9 +15,10 @@ from django.test.runner import DiscoverRunner
 from django.test.testcases import LiveServerThread, QuietWSGIRequestHandler
 from django.urls import ResolverMatch, resolve, reverse
 from sauceclient import SauceClient
-from selenium.webdriver import Firefox, Remote
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.webdriver import WebDriver as Firefox
+from selenium.webdriver.remote.webdriver import WebDriver as Remote
 from selenium.webdriver.safari.options import Options as SafariOptions
 from selenium.webdriver.support.expected_conditions import staleness_of
 from selenium.webdriver.support.ui import WebDriverWait
