b2e1d07450f4bca5adbef205868b1c92591d6df1
Daniel Brown <djbrown@users.noreply.github.com>
2026-06-20 00:42:36 +0200

    fix correct_data integration tests

--- src/base/tests/integration/test_correct_data.py
+++ src/base/tests/integration/test_correct_data.py
@@ -1,34 +1,18 @@
-import datetime
-import unittest
-
-from django.utils import timezone
-
 from base.tests.base import IntegrationTestCase
+from districts.models import District
 from games.models import Game
-from leagues.models import League
+from leagues.models import League, Season
 from players.models import Score
+from teams.models import Team
 
 
-@unittest.skip("broken integration test")
 class ImportGamesTest(IntegrationTestCase):
-    def test_game_210116_sghh_hcn(self):
-        self.assert_command("import_associations", "-a", 35)
-        self.assert_command("import_districts", "-d", 35)
-        self.assert_command("import_leagues", "-s", 2017, "-l", 26773)
-        league = self.assert_object(League)
-
-        self.assert_command("import_games", "-g", 210116)
-        game = self.assert_object(Game)
-
-        self.assertEqual(game.number, 210116)
-        self.assertEqual(game.opening_whistle, timezone.make_aware(datetime.datetime(2018, 2, 18, 17, 30)))
-        self.assertEqual(game.home_team.short_name, "SG Heidel/Helm")
-        self.assertEqual(game.guest_team.short_name, "HC Neuenbürg")
-        self.assertEqual(game.home_goals, 29)
-        self.assertEqual(game.guest_goals, 35)
-        self.assertIsNone(game.report_number)
-        self.assertEqual(game.sports_hall.number, 21004)
-        self.assertEqual(game.league, league)
+    def test_specific_game(self):
+        district = District.objects.create(bhv_id=1)
+        season = Season.objects.create(start_year=2)
+        league = League.objects.create(bhv_id=26773, district=district, season=season)
+        team = Team.objects.create(bhv_id=3, league=league)
+        Game.objects.create(number=210116, league=league, home_team=team, guest_team=team)
 
         self.assert_objects(Score, 0)
 
@@ -36,27 +20,9 @@ class ImportGamesTest(IntegrationTestCase):
 
         self.assert_objects(Score, 26)
 
-    def test_game_96781(self):
-        self.assert_command("import_associations", "-a", 3)
-        self.assert_command("import_districts", "-d", 3)
-        self.assert_command("import_leagues", "-s", 2019, "-l", 45956)
-        league = self.assert_object(League)
-
-        self.assert_command("import_games", "-g", 96781)
-        game = self.assert_object(Game)
-
-        self.assertEqual(game.number, 96781)
-        self.assertEqual(game.opening_whistle, timezone.make_aware(datetime.datetime(2019, 9, 14, 19, 00)))
-        self.assertEqual(game.home_team.short_name, "TV Weingarten")
-        self.assertEqual(game.guest_team.short_name, "SG Argental")
-        self.assertEqual(game.home_goals, 19)
-        self.assertEqual(game.guest_goals, 23)
-        self.assertIsNone(game.report_number)
-        self.assertEqual(game.sports_hall.number, 8092)
-        self.assertEqual(game.league, league)
-
+    def test_without_games(self):
         self.assert_objects(Score, 0)
 
         self.assert_command("correct_data")
 
-        self.assert_objects(Score, 27)
+        self.assert_objects(Score, 0)
