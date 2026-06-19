0a421b9603e8790cb361914be9ea6c08c6cb1337
Daniel Brown <djbrown@users.noreply.github.com>
2026-06-20 00:46:00 +0200

    fix districts integration tests

--- src/districts/tests/integration/test_import_districts.py
+++ src/districts/tests/integration/test_import_districts.py
@@ -1,33 +1,34 @@
-import unittest
-
+from associations.models import Association
 from base.tests.base import IntegrationTestCase
 from districts.models import District
 
 
-@unittest.skip("broken integration test")
 class CommandTest(IntegrationTestCase):
     def test_specific(self):
-        self.assert_command("import_associations", "-a", 35)
-        self.assert_command("import_districts", "-d", 35)
+        association = Association.objects.create(bhv_id=95)
+        self.assert_command("import_districts", "-d", 95)
         district = self.assert_object(District)
-        self.assertEqual(district.bhv_id, 35)
-        self.assertEqual(district.name, "Badischer Handball-Verband")
+        self.assertEqual(district.bhv_id, 95)
+        self.assertEqual(district.name, "Fédération Luxembourgeoise de Handball")
+        self.assertEqual(list(district.associations.all()), [association])
 
     def test_single_association(self):
-        self.assert_command("import_associations", "-a", 4, 35)
-        self.assert_command("import_districts", "-a", 4)
-        self.assert_objects(District, 4)
+        Association.objects.create(bhv_id=95, name="95")
+        association = Association.objects.create(bhv_id=76)
+        self.assert_command("import_districts", "-a", 76)
+        districts = self.assert_objects(District, 3)
+        self.assertEqual(set(association.district_set.all()), set(districts))
 
     def test_update(self):
-        District.objects.create(name="My District", bhv_id=35)
+        Association.objects.create(bhv_id=76)
+        District.objects.create(name="My District", bhv_id=12)
 
-        self.assert_command("import_associations", "-a", 35)
-        self.assert_command("import_districts", "-d", 35)
+        self.assert_command("import_districts", "-d", 12)
 
         district = self.assert_object(District)
-        self.assertEqual(district.name, "Badischer Handball-Verband")
+        self.assertEqual(district.name, "Bodensee-Donau")
 
     def test_all(self):
         self.assert_command("import_associations")
         self.assert_command("import_districts")
-        self.assert_objects(District, count=66)
+        self.assert_objects(District, count=4)
