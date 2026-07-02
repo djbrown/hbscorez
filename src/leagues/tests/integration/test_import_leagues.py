790da543fea31578aa9dc3aa500d3fa226eb24e4
Daniel Brown <djbrown@users.noreply.github.com>
2026-07-03 00:21:13 +0200

    fi season integration tests

--- src/leagues/tests/integration/test_import_leagues.py
+++ src/leagues/tests/integration/test_import_leagues.py
@@ -1,34 +1,42 @@
 import unittest
 
+from django.conf import settings
+
+from associations.models import Association
 from base.tests.base import IntegrationTestCase
 from districts.models import District
 from leagues.models import League, LeagueName, Season
 
 
-@unittest.skip("broken integration test")
 class SeasonTest(IntegrationTestCase):
     def test_specific(self):
-        self.assert_command("import_associations", "-a", 35)
-        self.assert_command("import_districts", "-d", 35)
-        self.assert_command("import_leagues", "-s", 2017, "-l", 0)
+        association = Association.objects.create(
+            bhv_id=76, source_url=f"{settings.NEW_ROOT_SOURCE_URL}/home/portal/vorarlberg"
+        )
+        District.objects.create(bhv_id=76).associations.add(association)
+        self.assert_command("import_leagues", "-s", 2017, "-l", 0, "--skip-teams")
 
         season = self.assert_object(Season)
         self.assertEqual(season.start_year, 2017)
 
     def test_all(self):
-        self.assert_command("import_associations", "-a", 4)
-        self.assert_command("import_districts", "-d", 3)
-        self.assert_command("import_leagues", "-l", 0)
+        association = Association.objects.create(
+            bhv_id=76, source_url=f"{settings.NEW_ROOT_SOURCE_URL}/home/portal/vorarlberg"
+        )
+        District.objects.create(bhv_id=76).associations.add(association)
+        self.assert_command("import_leagues", "-l", 0, "--skip-teams")
 
         self.assert_objects(League, 0)
-        for start_year in range(2004, 2023):
+        for start_year in range(2004, 2026):
             exists = Season.objects.filter(start_year=start_year).exists()
             self.assertTrue(exists, f"Season {start_year} should exist")
 
     def test_old_seasons(self):
-        self.assert_command("import_associations", "-a", 4)
-        self.assert_command("import_districts", "-d", 3)
-        self.assert_command("import_leagues", "-l", 0)
+        association = Association.objects.create(
+            bhv_id=95, source_url=f"{settings.NEW_ROOT_SOURCE_URL}/home/portal/luxemburg"
+        )
+        District.objects.create(bhv_id=95).associations.add(association)
+        self.assert_command("import_leagues", "-l", 0, "--skip-teams")
 
         self.assert_objects(League, count=0)
 
